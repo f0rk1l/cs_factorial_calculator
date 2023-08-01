67f9114abb1be356c1838a110e58300bba93f971
f0rk1l <ilia.yuskiv@mail.ru>
2023-08-02 05:33:42 +0600

    Initial commit

---
+++ FactorialCalculator/FactorialCalculator.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactorialCalculator
+{
+    class FactorialCalculator
+    {
+
+        private static bool IsPrime(double number)
+        {
+            for (int i = 2; i <= (int) Math.Sqrt(number); i++)
+            {
+                if (number % i == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private static double[] GetFib(int amount)
+        {
+            double[] Numbers = new double[amount];
+
+            Numbers[0] = 1;
+
+            Numbers[1] = 1;
+
+            for (int i = 2; i < amount; i++)
+            {
+                Numbers[i] = (Numbers[i - 1] + Numbers[i - 2]);
+            }
+
+            return Numbers;
+        }
+
+
+        public static double Factorial(double number)
+        {
+            int Result = 1;
+
+            for (int i = 1; i <= number; i++)
+            {
+                Result *= i;
+            }
+
+            return Result;
+        }
+
+        public static double DoubleFactorial(double number)
+        {
+            bool IsEven = number % 2 == 0;
+
+            double Result = 1;
+
+            for (int i = 1; i <= number; i++)
+            {
+                if ((i % 2 == 0) == IsEven)
+                {
+                    Result *= i;
+                }
+            }
+
+            return Result;
+        }
+
+        public static double Primorial(double number)
+        {
+            double Result = 1;
+
+            for (int i = 1; i <= number; i++)
+            {
+                if (IsPrime(i))
+                {
+                    Result *= i;
+                }
+
+            }
+
+            return Result;
+        }
+
+        public static double SuperFactorial(double number)
+        {
+            double Result = 1;
+
+            for (double i = 1; i <= number; i++)
+            {
+                Result *= Factorial(i);
+            }
+
+            return Result;
+        }
+
+        public static double Fibonarial(double number)
+        {
+            double Result = 1;
+
+            foreach (double i in GetFib((int) number))
+            {
+                Result *= i;
+            }
+
+            return Result;
+        }
+
+    }
+
+}
---
+++ FactorialCalculator/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactorialCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                try
+                {
+                    Console.Write("Enter number: ");
+                    double number = Convert.ToDouble(Console.ReadLine());
+
+                    Console.Write("\nChoose factorial type:\n\n[1] Factorial\n\n[2] Double factorial\n\n[3] Primorial\n\n[4] Super factorial\n\n[5] Fibonarial\n\nChoose: ");
+                    int Choose = Convert.ToInt32(Console.ReadLine());
+
+
+                    switch (Choose)
+                    {
+                        case 1:
+
+                            Console.WriteLine($"\n\nFactorial of {number}: {FactorialCalculator.Factorial(number)}");
+
+                            break;
+
+                        case 2:
+
+                            Console.WriteLine($"\n\nDouble factorial of {number}: {FactorialCalculator.DoubleFactorial(number)}");
+
+                            break;
+
+                        case 3:
+
+                            Console.WriteLine($"\n\nPrimorial of {number}: {FactorialCalculator.Primorial(number)}");
+
+                            break;
+
+                        case 4:
+
+                            Console.WriteLine($"\n\nSuper factorial of {number}: {FactorialCalculator.SuperFactorial(number)}");
+
+                            break;
+
+                        case 5:
+
+                            Console.WriteLine($"\n\nFibonarial of {number}: {FactorialCalculator.Fibonarial(number)}");
+
+                            break;
+
+                        default:
+
+                            Console.WriteLine("\n\nIncorrect choose!");
+
+                            break;
+                    }
+
+                } 
+                
+                catch(FormatException ex)
+                {
+                    Console.WriteLine("\n\nIncorrect input!");
+                }
+
+                Console.Write("\n\nPress some button to clear output...");
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+    }
+}
